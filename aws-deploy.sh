275b753a508dbf1e604a3baafce2fde0fd7de5a7
Kai Page <kai@quantaly.net>
2023-12-20 13:19:37 -0600

    Use the -v flag when building

--- aws-deploy.sh
+++ aws-deploy.sh
@@ -2,7 +2,7 @@
 set -x
 TMPDIR=$(mktemp -d)
 
-GOOS=linux GOARCH=arm64 go build -trimpath -ldflags "-s" -o $TMPDIR/bootstrap github.com/Mines-Little-Theatre/team-trivia-scraper/aws-lambda && \
+GOOS=linux GOARCH=arm64 go build -v -trimpath -ldflags "-s" -o $TMPDIR/bootstrap github.com/Mines-Little-Theatre/team-trivia-scraper/aws-lambda && \
 zip -j $TMPDIR/deploy.zip $TMPDIR/bootstrap && \
 aws lambda update-function-code --function-name $FUNCTION_NAME --zip-file fileb://$TMPDIR/deploy.zip --no-cli-pager
 
