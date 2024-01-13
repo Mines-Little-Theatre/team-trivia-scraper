9c17538aa7a3dedac3a7139d6322d49ddb4836e8
Kai Page <kai@quantaly.net>
2024-01-12 18:12:04 -0700

    Stop parameterizing the function name in the deploy script
    it was a dumb idea anyway

--- aws-deploy.sh
+++ aws-deploy.sh
@@ -4,6 +4,6 @@ TMPDIR=$(mktemp -d)
 
 GOOS=linux GOARCH=arm64 go build -v -trimpath -ldflags "-s" -o $TMPDIR/bootstrap github.com/Mines-Little-Theatre/team-trivia-scraper/aws-lambda && \
 zip -j $TMPDIR/deploy.zip $TMPDIR/bootstrap && \
-aws lambda update-function-code --function-name $FUNCTION_NAME --zip-file fileb://$TMPDIR/deploy.zip --no-cli-pager
+aws lambda update-function-code --function-name mlt-trivia-bot --zip-file fileb://$TMPDIR/deploy.zip --no-cli-pager
 
 rm -Rf $TMPDIR
